f0d14154a501f8be7ec81c8b2234ad1d4bdd1cbe
Wahyu Bagus <101736756+wahyubagusa@users.noreply.github.com>
2025-09-17 08:31:38 +0700

    layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,18 +1,14 @@
+// src/app/layout.js
 import "./globals.css";
-export const metadata = { title: "Databuyer", description: "Input Data Order" };
+
+export const metadata = {
+  title: "Databuyer",
+  description: "Input Data Order",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="id">
-      <head>
-        {/* Google Font: Montserrat */}
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap"
-          rel="stylesheet"
-        />
-      </head>
       <body className="app-body">{children}</body>
     </html>
   );
